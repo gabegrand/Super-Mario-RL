02ff80360bf6d4ae0fae4714abe0cd88a15e52ac
Kevin Loughlin <kevinloughlin@college.harvard.edu>
2016-12-03 15:07:51 -0500

    Fixed off by one error in file load

--- test.py
+++ test.py
@@ -26,7 +26,7 @@ if hp.LOAD_FROM is not None:
     agent.load(hp.LOAD_FROM)
 
     # Start iterations from where we left off
-    j = int(hp.LOAD_FROM[hp.LOAD_FROM.rfind('-')+1:hp.LOAD_FROM.rfind('.pickle')]) + 1
+    j = int(hp.LOAD_FROM[hp.LOAD_FROM.rfind('-')+1:hp.LOAD_FROM.rfind('.pickle')])
     print('Starting at iteration %d' % j)
 
 else:
