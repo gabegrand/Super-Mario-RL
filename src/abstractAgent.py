71e853014420704a5d87021d0a9c38ccd7175c59
Kevin Loughlin <kevinloughlin@college.harvard.edu>
2016-12-10 16:26:24 -0500

    Added abstract agent

---
+++ src/abstractAgent.py
@@ -0,0 +1,36 @@
+import util
+import hyperparameters as hp
+import numpy as np
+import pickle
+from datetime import datetime
+import random
+import features as feat
+
+class AbstractAgent:
+
+    def getActionAndUpdate(self, state, reward):
+        raise NotImplementedError()
+
+    def reset(self):
+        raise NotImplementedError()
+
+    def getN(self, state, action):
+        raise NotImplementedError()
+
+    def incN(self, state, action):
+        raise NotImplementedError()
+
+    def getQ(self, state, action):
+        raise NotImplementedError()
+
+    def computeValueFromQValues(self, state):
+        raise NotImplementedError()
+
+    def computeActionFromQValues(self, state):
+        raise NotImplementedError()
+
+    def save(self, i, j, diagnostics):
+        raise NotImplementedError()
+
+    def load(self, fname):
+        raise NotImplementedError()
